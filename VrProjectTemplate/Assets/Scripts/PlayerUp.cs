59f7aafb5aff058a4dd306c68006d5baf687cb77
Aang121 <123459150+Aang1214@users.noreply.github.com>
2023-02-20 23:21:09 +0000

    AddMoreToSingle

---
+++ VrProjectTemplate/Assets/Scripts/PlayerUp.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class PlayerUp : MonoBehaviour
+{
+    public GameObject water;
+    private int up = 5;
+    private Rigidbody rb;
+    public bool upplease = false;
+    private bool OnWater;
+    public TextMeshProUGUI countText;
+    private int count;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        upplease = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && upplease)
+        {
+            rb.velocity = Vector3.up * up;
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            water.SetActive(true);
+        }
+        if (Input.GetKey(KeyCode.G))
+        {
+            water.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("ladder"))
+        {
+            upplease = true;
+        }
+
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("ladder"))
+        {
+            upplease = false;
+        }
+    }
+
+}
+
--- VrProjectTemplate/Assets/Scripts/firedes.cs
+++ VrProjectTemplate/Assets/Scripts/firedes.cs
@@ -2,20 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class firedes : MonoBehaviour
 {
  public bool IsOnFire = true;
+ public float OnFire = 100f;
+ public TextMeshProUGUI countFire;
+ private int count;
   
     public ParticleSystem part;
     void Start()
     {
+        count = 0;
         part = GetComponent<ParticleSystem>();
+    }
+    void SetCountText()
+    {
+        countFire.text = "Fire " + count.ToString();
+        if (count >= 8)
+        {
+            
+        }
     }
     public void OnParticleCollision(GameObject other)
     {
         if (other.gameObject.CompareTag("Fire"))
         {
-            other.SetActive(false);
+            OnFire -= 1f;
+            other.transform.localScale -= new Vector3(0.1f, 0.3f, 0.1f);
+            if (OnFire <= 0)
+            {
+                other.SetActive(false);
+                count = count + 1;
+            }
         }
     }
 }
