629c3f78e26c521fccbc9672261a4bde524b95c6
Aang121 <123459150+Aang1214@users.noreply.github.com>
2023-02-25 21:12:29 +0000

    AddEnvironmentToVr

--- VrProjectTemplate/Assets/Scripts/firedes.cs
+++ VrProjectTemplate/Assets/Scripts/firedes.cs
@@ -1,39 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+
 public class firedes : MonoBehaviour
 {
- public bool IsOnFire = true;
- public float OnFire = 100f;
- public TextMeshProUGUI countFire;
- private int count;
-  
+    public float FireHealth = 100f;
     public ParticleSystem part;
     void Start()
     {
-        count = 0;
         part = GetComponent<ParticleSystem>();
-    }
-    void SetCountText()
-    {
-        countFire.text = "Fire " + count.ToString();
-        if (count >= 8)
-        {
-            
-        }
     }
     public void OnParticleCollision(GameObject other)
     {
         if (other.gameObject.CompareTag("Fire"))
         {
-            OnFire -= 1f;
+            FireHealth -= 1f;
             other.transform.localScale -= new Vector3(0f, 0.3f, 0f);
-            if (OnFire <= 0)
+            if (FireHealth <= 0)
             {
                 other.SetActive(false);
-                count = count + 1;
             }
         }
     }
