7657ea1347f8de1bad1ac7f295c71a11ff0d6783
Aang121 <123459150+Aang1214@users.noreply.github.com>
2023-02-28 13:33:25 +0000

    FireHealth

--- VrProjectTemplate/Assets/Scripts/FireHealth.cs
+++ VrProjectTemplate/Assets/Scripts/FireHealth.cs
@@ -6,22 +6,5 @@ using UnityEngine.UI;
 public class FireHealth : MonoBehaviour
 {
     public float FireHealt = 100f;
-    public ParticleSystem part;
     
-    void Start()
-    {
-        part = GetComponent<ParticleSystem>();
-    }
-    public void OnParticleCollision(GameObject other)
-    {
-        if (other.gameObject.CompareTag("Fire"))
-        {
-            FireHealt -= 1f;
-            other.transform.localScale -= new Vector3(0f, 0.3f, 0f);
-            if (FireHealt <= 0)
-            {
-                other.SetActive(false);
-            }
-        }
-    }
 }
